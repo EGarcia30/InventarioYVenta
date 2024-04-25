e8f3f800817bd845c3554851d2c59a6e6272114e
Erick Penado <penado188@gmail.com>
2024-04-25 02:34:19 -0600

    ajustando PL y agregando Tailwindcss

---
+++ InventarioYVenta.PL/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "class",
+    content: ["./Views/**/*.cshtml"],
+    theme: {
+        extend: {
+            maxWidth: {
+                'nav': '1280px',
+            },
+            gridTemplateColumns: {
+                'responsive': 'repeat(auto-fill, minmax(15rem, 1fr))'
+            }
+        },
+    },
+    plugins: [],
+}
+
