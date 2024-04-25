6769203fd6e2f7fadaced3e216907c54e3f3b5e0
Erick Penado <penado188@gmail.com>
2024-04-25 11:28:31 -0600

    Agregando modelos login y messageError

---
+++ InventarioYVenta.Models/ViewModels/LoginVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioYVenta.Models.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "El nombre de usuario es requerido.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerido.")]
+        public string? Password { get; set; }
+    }
+}
